4372eb8220ae8df6658965548991fdd23b8dc556
Megan Grisley <megangrisley@gmail.com>
2015-05-28 12:03:03 -0600

    Created user input method

--- magic_eight.rb
+++ magic_eight.rb
@@ -1,3 +1,18 @@
+# Magic Eight Ball
+# Basic Objectives:
+# User inputs question – (push & switch drivers)
+# Computer outputs random answer – (push & switch drivers)
+# User inputs "QUIT” – (push & switch drivers)
+# Computer outputs a goodbye message and exits – (push & switch drivers)
+
+puts "Welcome to the Magic Eightball"
+puts "What is you question?"
+choice = gets.chomp
+
 class MagicEight
   
+  def get_input
+    gets 
+  end
+
 end
