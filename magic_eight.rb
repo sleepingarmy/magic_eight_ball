2c50d69f268dd7eb2c41c052ce6621918e1bcbe8
Rosemary Thomas <rsm.thomas@gmail.com>
2015-05-28 12:10:13 -0600

    added computer random output

--- magic_eight.rb
+++ magic_eight.rb
@@ -4,15 +4,38 @@
 # Computer outputs random answer – (push & switch drivers)
 # User inputs "QUIT” – (push & switch drivers)
 # Computer outputs a goodbye message and exits – (push & switch drivers)
-
-puts "Welcome to the Magic Eightball"
-puts "What is you question?"
-choice = gets.chomp
-
 class MagicEight
-  
+
+  choice = gets.chomp
   def get_input
-    gets 
+    puts "Welcome to the Magic Eightball"
+    puts "What is you question?"
+    choice = gets.chomp
   end
 
+  def output_random
+    answers = ["It is certain", 
+      "It is decidedly so", 
+    "Without a doubt", 
+    "Yes definitely", 
+    "You may realy on it", 
+    "As I see it, yes", 
+    " Most likely", 
+    "Outlook good", 
+    "Yes", 
+    "Signs point to yes", 
+    "Reply hazy, try again", 
+    "Ask again later", 
+    "Better not tell you now...", 
+    "Cannot predict now", 
+    "Concentrate and ask again", 
+    "Don't count on it", 
+    "My reply is no", 
+    "My sources say no", 
+    "Outlook not so good", 
+    "Very doubtful"]
+    output = answers.sample
+    put "#{ output }"
+  end
+  
 end
